ebb1cd142ed273fc732f2cecfa5be2f56b9f7c25
vmz <viemvema@gmail.com>
2026-03-21 09:54:14 -0600

    arreglando mocks para dejarlos como datos de prueba iniciales

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,6 +1,19 @@
-export const projects = [
-  { id: 'p1', name: 'Rediseño web', status: 'active' },
-  { id: 'p2', name: 'App móvil', status: 'active' },
-  { id: 'p3', name: 'Onboarding interno', status: 'archived' },
-];
-``
+/**
+ * RESPONSABLE: Victor
+ *
+ * Simula la base de datos de proyectos.
+ *
+ * Este array será utilizado por:
+ * projectService.ts
+ */
+
+import { Project } from "../types/Project"
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    name: "Proyecto Demo",
+    description: "Proyecto de ejemplo",
+    ownerId: 1
+  }
+]
--- src/mocks/tasks.ts
+++ src/mocks/tasks.ts
@@ -1,6 +1,22 @@
-export const tasks = [
-  { id: 't1', title: 'Mejorar Frontend', projectId: 'p1', status: 'pending', dueDate: '2026-03-20' },
-  { id: 't2', title: 'CI/CD', projectId: 'p1', status: 'in_progress', dueDate: '2026-03-22' },
-  { id: 't3', title: 'Definir mejoras', projectId: 'p2', status: 'pending', dueDate: '2026-03-21' },
-  { id: 't4', title: 'QA pagos', projectId: 'p2', status: 'done' },
-];
+/**
+ * RESPONSABLE: Victor
+ *
+ * Simula la base de datos de tareas.
+ *
+ * Este array será utilizado por:
+ * taskService.ts
+ */
+
+
+
+import { Task } from "../types/Task"
+
+export const tasks: Task[] = [
+  {
+    id: 1,
+    title: "Crear interfaz",
+    projectId: 1,
+    assignedTo: 2,
+    status: "pending"
+  }
+]
